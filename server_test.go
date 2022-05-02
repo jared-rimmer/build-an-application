5af7bcfa93a1e52e99c5cbbcfca3ec60dab400cb
jared.rimmer <jared.rimmer1+github@gmail.com>
2022-05-02 17:22:09 +0100

    Apply DRY logic to server_test.go

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,34 +11,36 @@ func TestGETPlayers(t *testing.T) {
 
 	t.Run("returns the score for Pepper", func(t *testing.T) {
 
-		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		request := newGetStoreRequest("Pepper")
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
 
-		got := response.Body.String()
-		want := "20"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertResponseBody(t, response.Body.String(), "20")
 
 	})
 
 	t.Run("returns the score for Floyd", func(t *testing.T) {
 
-		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		request := newGetStoreRequest("Floyd")
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
 
-		got := response.Body.String()
-		want := "10"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertResponseBody(t, response.Body.String(), "10")
 
 	})
 
+}
+
+func newGetStoreRequest(name string) *http.Request {
+	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return request
+}
+
+func assertResponseBody(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q want %q", got, want)
+	}
 }
