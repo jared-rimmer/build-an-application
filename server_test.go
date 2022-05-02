ee860cb27201d8a44fcbc821782523ce3a0308f2
jared.rimmer <jared.rimmer1+github@gmail.com>
2022-05-02 17:12:35 +0100

    Add failing test for a second player

--- server_test.go
+++ server_test.go
@@ -7,16 +7,37 @@ import (
 )
 
 func TestGETPlayers(t *testing.T) {
-	request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
-	response := httptest.NewRecorder()
 
-	PlayerServer(response, request)
+	t.Run("returns the score for Pepper", func(t *testing.T) {
 
-	got := response.Body.String()
-	want := "20"
+		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+	})
+
+	t.Run("returns the score for Floyd", func(t *testing.T) {
+
+		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "10"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+	})
 
 }
